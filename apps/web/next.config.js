409fc2a45771912c036183ad7bf9e188bbb7cce5
Jan Piotrowski <piotrowski+github@gmail.com>
2023-01-30 18:36:56 +0000

    workaround for ENOENT schema.prisma

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,20 @@
-module.exports = {
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   transpilePackages: ["ui"],
-};
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (isServer) {
+      config.externals = [...config.externals, '@prisma/client']
+    }
+
+    return config
+  },
+}
+
+module.exports = nextConfig
